a525533aa420998dd384aea4376814c52dc9ae6c
Touheda Khanom <TouhedaKhanom14@gmail.com>
2022-10-09 11:36:14 -0400

    Initial commit

---
+++ Binary Search.py
@@ -0,0 +1,16 @@
+arr = [-2,3,4,7,8,9,11,13]
+target = 11
+#search(arr,target)
+
+def search(arr,target):
+    left = 0
+    right = arr.length -1
+    while left <= right:
+        mid = (left + right)/2
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
